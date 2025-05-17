acf4cecbf3ae23a6ac67aa4f8061ce6634ee931f
Ali Mohamed <162573315+AliMohamed805@users.noreply.github.com>
2025-05-17 15:24:49 +0300

    Update urls.py

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path , include
+# favorites/urls.py
+from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('addfavorites/', views.AddFavorites.as_view(), name='addfavorites'),
-
-    path('getfavorites/', views.GetFavorites.as_view(), name='getfavorites'),
-
-    path('deletefavorites/', views.DeleteFavorites.as_view(), name='deletefavorites'),
+    path('addfavorites/', views.add_favorite, name='add_favorite'),
+    path('deletefavorites/', views.delete_favorite, name='delete_favorite'),
+    path('getfavorites/', views.get_favorites, name='get_favorites'),
 ]
